0c691e06df9259ad76b778e7ebf923cf7eeae4ef
Paul Riseborough <p_riseborough@live.com.au>
2017-02-01 21:32:17 +1100

    events: add baro thermal calibration method

--- src/modules/events/temperature_calibration.h
+++ src/modules/events/temperature_calibration.h
@@ -42,3 +42,6 @@ int run_temperature_calibration();
  * @return 0 on success, <0 error otherwise  */
 int run_temperature_accel_calibration();
 
+/** start baro temperature calibration in a new task
+ * @return 0 on success, <0 error otherwise  */
+int run_temperature_baro_calibration();
